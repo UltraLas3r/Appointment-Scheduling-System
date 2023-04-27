21de94cd4322131b1f3367ea280418587c383607
Michael Peter Schreiber <mschreiber@its4logistics.com>
2023-04-27 10:43:25 -0700

    some login stuff

--- Model/LogUserActivity.cs
+++ Model/LogUserActivity.cs
@@ -10,13 +10,25 @@ namespace mschreiber_Software2_c969Project.Model
 {
     internal class LogUserActivity
     {
+
         public static void ActivateLog(string userName)
         {
             // Get the current date and time
             DateTime currentTime = DateTime.Now;
+            bool LoginError = false;
 
             // Construct the log message
+
             string logMessage = $"The User {userName} logged in at {currentTime}";
+            string selectQuery = $"SELECT COUNT(*) FROM users WHERE username = '{userName}'";
+
+            //TODO fix this error checking for if the username exists or not
+            
+            if (selectQuery != userName)
+            {
+                string ErrorLogMessage = $"Failed Attempt using login {userName}.\nAt {currentTime}";
+                LoginError = true;
+            }
 
             // Define the file path for the log file
             string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
@@ -29,13 +41,16 @@ namespace mschreiber_Software2_c969Project.Model
             // If the log file exists, append the new log message to the end of the file
             if (logFileExists)
             {
-                File.AppendAllText(logFilePath, logMessage + Environment.NewLine);
+                File.AppendAllText(logFilePath, logMessage + Environment.NewLine);   
             }
             // otherwise...
             else
             {
                 File.WriteAllText(logFilePath, logMessage + Environment.NewLine);
             }
+
+            if (logFileExists && LoginError == true ) 
+            { File.AppendAllText(logFilePath, ErrorlogMessage + Environment.NewLine); }
         }
     }
 }
